4e0a89fcdc4bbcc19e443a0299ef62cc2e43069c
Delaroy Studios <bamoyk@yahoo.com>
2016-08-23 01:30:36 -0700

    first commit

---
+++ app/src/main/java/com/delaroystudios/residemenu/ProfileFragment.java
@@ -0,0 +1,18 @@
+package com.delaroystudios.residemenu;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.delaroystudios.residemenu.R;
+
+public class ProfileFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile, container, false);
+    }
+
+}
